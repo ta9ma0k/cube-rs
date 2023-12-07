66dc35d1d87dc2326269257fd713d69a5fd97e69
okada takuma <takuma@pop-os.localdomain>
2023-12-07 21:53:50 +0900

    wip base cube struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,120 +1,68 @@
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-enum CornerPart {
-    ULB,
-    URB,
-    URF,
-    ULF,
-    DLB,
-    DRB,
-    DRF,
-    DLF,
+/// Up, Front, Left, Right, Back, Down
+///          + - - - +
+///          | * * * |
+///          | * U * |
+///          | * * * |
+///  + - - - + - - - + - - - + - - - +
+///  | * * * | * * * | * * * | * * * |
+///  | * L * | * F * | * R * | * B * |
+///  | * * * | * * * | * * * | * * * |
+///  + - - - + - - - + - - - + - - - +
+///          | * * * |
+///          | * D * |
+///          | * * * |
+///          + - - - +
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub enum CornerPart {
+    URF = 0,
+    UFL = 1,
+    ULB = 2,
+    UBR = 3,
+    DFR = 4,
+    DLF = 5,
+    DBL = 6,
+    DRB = 7,
 }
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-enum CornerOrientation {
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub enum CornerOrientation {
     FRONT = 0,
     RIGHT = 1,
     LEFT = 2,
 }
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-enum EdgePart {
-    LB,
-    BR,
-    RF,
-    FL,
-    UB,
-    UR,
-    UF,
-    UL,
-    DB,
-    DR,
-    DF,
-    DL,
-}
-
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-enum EdgeOrientation {
-    FRONT = 0,
-    TURNED = 1,
-}
-
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-struct Move {
-    cp: [CornerPart; 8],
-    co: [CornerOrientation; 8],
-    ep: [EdgePart; 12],
-    eo: [EdgeOrientation; 12],
-}
-
-const MOVE_R: Move = Move {
-    cp: [CornerPart::ULB, CornerPart::URB, CornerPart::URF, CornerPart::ULF, CornerPart::DLB, CornerPart::DRB, CornerPart::DRF, CornerPart::DLF],
-    co: [CornerOrientation::FRONT; 8],
-    ep: [EdgePart::LB, EdgePart::BR, EdgePart::RF, EdgePart::FL, EdgePart::UB, EdgePart::UR, EdgePart::UF, EdgePart::UL, EdgePart::DB, EdgePart::DR, EdgePart::DF, EdgePart::DL],
-    eo: [EdgeOrientation::FRONT; 12],
-};
-
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-struct Cube {
-    cp: [CornerPart; 8],
-    co: [CornerOrientation; 8],
-    ep: [EdgePart; 12],
-    eo: [EdgeOrientation; 12],
+#[allow(dead_code)]
+#[derive(Debug, Clone, PartialEq)]
+pub struct Cube {
+    corner_parts: [CornerPart; 8],
+    corner_orientations: [CornerOrientation; 8],
+    edge_parts:Vec<u8>,
+    edge_orientations: Vec<u8>,
 }
 
 #[allow(dead_code)]
 impl Cube {
-    fn new(cp: [CornerPart; 8], co: [CornerOrientation; 8], ep: [EdgePart; 12], eo: [EdgeOrientation; 12]) -> Cube {
-        Cube { cp, co, ep, eo }
-    }
-
-    fn solved() -> Self {
-        Cube {
-            cp : [CornerPart::ULB, CornerPart::URB, CornerPart::URF, CornerPart::ULF, CornerPart::DLB, CornerPart::DRB, CornerPart::DRF, CornerPart::DLF],
-            co : [CornerOrientation::FRONT; 8],
-            ep : [EdgePart::LB, EdgePart::BR, EdgePart::RF, EdgePart::FL, EdgePart::UB, EdgePart::UR, EdgePart::UF, EdgePart::UL, EdgePart::DB, EdgePart::DR, EdgePart::DF, EdgePart::DL],
-            eo : [EdgeOrientation::FRONT; 12],
-        }
+    pub fn new(
+        corner_parts: [CornerPart; 8],
+        corner_orientations: [CornerOrientation; 8],
+        edge_parts: Vec<u8>,
+        edge_orientations: Vec<u8>,
+    ) -> Self {
+        Self { corner_parts, corner_orientations, edge_parts, edge_orientations }
     }
 
     fn is_solved(&self) -> bool {
-        self.cp == [CornerPart::ULB, CornerPart::URB, CornerPart::URF, CornerPart::ULF, CornerPart::DLB, CornerPart::DRB, CornerPart::DRF, CornerPart::DLF] &&
-        self.co == [CornerOrientation::FRONT; 8] &&
-        self.ep == [EdgePart::LB, EdgePart::BR, EdgePart::RF, EdgePart::FL, EdgePart::UB, EdgePart::UR, EdgePart::UF, EdgePart::UL, EdgePart::DB, EdgePart::DR, EdgePart::DF, EdgePart::DL] &&
-        self.eo == [EdgeOrientation::FRONT; 12]
-    }
-
-    fn apply_move(&mut self, m: Move) {
-        self.cp = m.cp.map(|x| self.cp[x as usize]);
-        self.co = m.co.iter().enumerate().map(|(i, x)| {
-
-            match (x, self.co[i]) {
-                (CornerOrientation::FRONT, CornerOrientation::FRONT) => CornerOrientation::FRONT,
-                (CornerOrientation::FRONT, CornerOrientation::RIGHT) => CornerOrientation::RIGHT,
-                (CornerOrientation::FRONT, CornerOrientation::LEFT) => CornerOrientation::LEFT,
-                (CornerOrientation::RIGHT, CornerOrientation::FRONT) => CornerOrientation::RIGHT,
-                (CornerOrientation::RIGHT, CornerOrientation::RIGHT) => CornerOrientation::LEFT,
-                (CornerOrientation::RIGHT, CornerOrientation::LEFT) => CornerOrientation::FRONT,
-                (CornerOrientation::LEFT, CornerOrientation::FRONT) => CornerOrientation::LEFT,
-                (CornerOrientation::LEFT, CornerOrientation::RIGHT) => CornerOrientation::FRONT,
-                (CornerOrientation::LEFT, CornerOrientation::LEFT) => CornerOrientation::RIGHT,
-            }
-        }).collect::<Vec<CornerOrientation>>().try_into().unwrap();
-        self.ep = m.ep.map(|x| self.ep[x as usize]);
-        self.eo = m.eo.iter().enumerate().map(|(i, x)| {
-
-            match (x, self.eo[i]) {
-                (EdgeOrientation::FRONT, EdgeOrientation::FRONT) => EdgeOrientation::FRONT,
-                (EdgeOrientation::FRONT, EdgeOrientation::TURNED) => EdgeOrientation::TURNED,
-                (EdgeOrientation::TURNED, EdgeOrientation::FRONT) => EdgeOrientation::TURNED,
-                (EdgeOrientation::TURNED, EdgeOrientation::TURNED) => EdgeOrientation::FRONT,
-            }
-        }).collect::<Vec<EdgeOrientation>>().try_into().unwrap();
-    }
-
-    fn move_r(mut self) -> Self {
-        self.apply_move(MOVE_R);
-        self
+        self.corner_parts == [
+            CornerPart::URF,
+            CornerPart::UFL,
+            CornerPart::ULB,
+            CornerPart::UBR,
+            CornerPart::DFR,
+            CornerPart::DLF,
+            CornerPart::DBL,
+            CornerPart::DRB,
+        ]
+            && self.corner_orientations.iter().all(|&x| x == CornerOrientation::FRONT)
     }
 }
 
@@ -122,20 +70,53 @@ impl Cube {
 mod tests {
     use super::*;
 
+    const SERIAL_CORNER_PARTS: [CornerPart; 8] = [
+        CornerPart::URF,
+        CornerPart::UFL,
+        CornerPart::ULB,
+        CornerPart::UBR,
+        CornerPart::DFR,
+        CornerPart::DLF,
+        CornerPart::DBL,
+        CornerPart::DRB,
+    ];
+
     #[test]
-    fn test_is_solved() {
-        let cube = Cube::solved();
+    fn cube_is_solved_should_return_true() {
+        let cube = Cube::new(
+            SERIAL_CORNER_PARTS,
+            [CornerOrientation::FRONT; 8],
+            vec![], vec![]
+        );
         assert!(cube.is_solved());
     }
 
     #[test]
-    fn test_move_r() {
-        let cube = Cube::solved();
-        let moved_cube = cube.move_r();
+    fn cube_is_solved_should_return_false_when_corner_parts_is_not_serial_number() {
+        let cube = Cube::new(
+[
+        CornerPart::ULB,
+        CornerPart::UBR,
+        CornerPart::DFR,
+        CornerPart::DLF,
+        CornerPart::DBL,
+        CornerPart::DRB,
+        CornerPart::URF,
+        CornerPart::UFL,
+    ],
+            [CornerOrientation::FRONT; 8],
+            vec![], vec![]
+        );
+        assert!(!cube.is_solved());
+    }
 
-        assert_eq!(moved_cube.cp, [CornerPart::ULB, CornerPart::URB, CornerPart::URF, CornerPart::ULF, CornerPart::DLB, CornerPart::DRB, CornerPart::DRF, CornerPart::DLF]);
-        assert_eq!(moved_cube.co, [CornerOrientation::FRONT; 8]);
-        assert_eq!(moved_cube.ep, [EdgePart::LB, EdgePart::BR, EdgePart::RF, EdgePart::FL, EdgePart::UB, EdgePart::UR, EdgePart::UF, EdgePart::UL, EdgePart::DB, EdgePart::DR, EdgePart::DF, EdgePart::DL]);
-        assert_eq!(moved_cube.eo, [EdgeOrientation::FRONT; 12]);
+    #[test]
+    fn cube_is_solved_should_return_false_when_corner_orientations_is_not_front() {
+        let cube = Cube::new(
+            SERIAL_CORNER_PARTS,
+            [CornerOrientation::LEFT; 8],
+            vec![], vec![]
+        );
+        assert!(!cube.is_solved());
     }
 }
